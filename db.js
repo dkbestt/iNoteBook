94e25b8cbd4756737d3d0c25470298cb0e3d6fe2
Divyang Kanpariya <dk.bestt@gmail.com>
2022-10-06 21:24:13 +0530

    first commit

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
 const connection = () => {
-    mongoose.connect('mongodb://localhost:27017/inotebook', {
+    // mongoose.connect('mongodb://localhost:27017/inotebook', {
+    mongoose.connect('mongodb+srv://dkbestt:jAn3htqGXyr0axUA@cluster0.7a5st3u.mongodb.net/inotebook?retryWrites=true&w=majority', {
         // useCreateIndex: true,
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        // useFindAndModify: false
     }).then(() => {
         console.log('Connection Successfull...');
     }).catch((err) => {
@@ -13,3 +15,6 @@ const connection = () => {
 }
 
 module.exports = connection
+
+// jAn3htqGXyr0axUA
+// inotebook
